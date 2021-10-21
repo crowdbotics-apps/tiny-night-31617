9106691761a5a1a454d95025261066225ee4a7e7
Crowdbotics <team@crowdbotics.com>
2021-10-21 15:07:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tiny_night_31617",
+  url: "https://tiny_night_31617.botics.co",
+  api: "https://tiny_night_31617.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
